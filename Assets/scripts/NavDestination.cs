7edee171ddf2be08fb367cbe25ce8d2bffd648b3
badaueba <alan.develop@outlook.com>
2017-11-06 15:57:47 -0200

    init and navmesh area ready

---
+++ Assets/scripts/NavDestination.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavDestination : MonoBehaviour {
+
+	public Transform destination; 
+	NavMeshAgent navMeshAgent;
+
+	// Use this for initialization
+	void Start () {
+		navMeshAgent = this.GetComponent<NavMeshAgent> ();
+		Go ();
+	}
+	
+	void Go() {
+		if (destination != null) {
+			Vector3 pos = destination.position;
+			navMeshAgent.SetDestination (pos);
+		}
+	}
+}
