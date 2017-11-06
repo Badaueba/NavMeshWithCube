547ded5f12b8ae36fb6e81a523658854e9369a40
badaueba <alan.develop@outlook.com>
2017-11-06 18:13:15 -0200

    click point destination

---
+++ Assets/scripts/DestinationController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestinationController : MonoBehaviour {
+
+	Vector3 mousePosition, targetPosition;
+	public AgentController agent;
+
+	void FixedUpdate() {
+
+		RaycastHit hit;
+		if (Input.GetMouseButtonDown (0)) {
+			if (Physics.Raycast (Camera.main.ScreenPointToRay (Input.mousePosition), out hit, 100)) {
+				print ("hit point: " + hit.point);
+				transform.position = new Vector3 (hit.point.x, 0f, hit.point.z);
+				agent.Go ();
+			}
+		}
+	}
+}
